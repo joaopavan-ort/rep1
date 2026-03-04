4338a42257092b670f88b2f2931f23cd6546334a
joaopavan-ort <joao.pavan@ort.org.br>
2026-03-04 14:31:14 -0300

    Update Ex8.py

--- lista1/Ex8.py
+++ lista1/Ex8.py
@@ -1,5 +1,4 @@
 # Lista 1-8
-# Não consegui resolver corretamente, mas fiz o que eu sabia fazer
 print('Exercício 8')
 m = int(input('Informe um valor em reais, desconsiderando centavos: '))
 c100 = m // 100
@@ -8,14 +7,14 @@ c20 = ((m % 100) % 50) // 20
 c10 = (((m % 100) % 50) % 20) // 10
 c5 = ((((m % 100) % 50) % 20) % 10) // 5
 c2 = (((((m % 100) % 50) % 20) % 10) % 5) // 2
-total = (c100 * 100) + (c50 * 50) + (c20 * 20) + (c10 * 10) + (c5 * 5) + (c2 * 2)
+m1 = ((((((m % 100) % 50) % 20) % 10) % 5) % 2)
+total = (c100 * 100) + (c50 * 50) + (c20 * 20) + (c10 * 10) + (c5 * 5) + (c2 * 2) + m1
 print('100:', c100)
 print('50:', c50)
 print('20:', c20)
 print('10:', c10)
 print('5:', c5)
 print('2:', c2) 
+print('1:', m1)
 print('Total:', total)
-print('Nº de Notas Usadas:', c100 + c50 + c20 + c10 + c5 + c2)
-if (m - total) > 0:
-    print('Deu errado, sobrou', m - total)
+print('Nº de Notas Usadas:', c100 + c50 + c20 + c10 + c5 + c2 + m1)
