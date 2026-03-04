c02481c174f0c06a7dcf62763f86a517adaa8824
joaopavan-ort <joao.pavan@ort.org.br>
2026-03-04 14:51:47 -0300

    ajustando 6 e 7

--- lista1/Ex6.py
+++ lista1/Ex6.py
@@ -2,7 +2,6 @@
 print('Exercício 6')
 X = float(input('Escolha um valor para X: '))
 Y = float(input('Agora escolha um valor para Y: '))
-X, Y = map(float, input('Escolha 2 valores para X e Y ').split())
 print('Agora vou invertê-los')
 print('')
 print('invertendo...')
--- lista1/Ex7.py
+++ lista1/Ex7.py
@@ -1,4 +1,9 @@
 # Lista 1-7
 print('Exercício 7')
-hora, minuto = map(int, input('Que horas são? ').split())
+tempo = input('Que horas são? [00:00] ')
+hora = int(tempo[0:2])
+minuto = int(tempo[3:5])
+print('')
+print('Horas:', hora)
+print('Minutos:', minuto)
 print('Se passaram', (hora * 60) + minuto, 'minutos desde o início do dia.')
